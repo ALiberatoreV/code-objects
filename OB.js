14aefe811aa8ff4c2779fa80b9a415733e5e3e47
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-11-15 11:37:44 -0500

    first commit

---
+++ OB.js
@@ -0,0 +1,33 @@
+let topMovies = [
+    {
+        name: "The Empire Strikes Back",
+        year: "1980",
+        director: " Irvin Kershner",
+        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/815oLrM3hqL._AC_SY879_.jpg"></Image>
+    },
+    {
+        name: "Avengers: Infinity War",
+        year: "2018",
+        director: "Anthony Russo, Joe Russo",
+        picture: <Image src ="https://cdn.shopify.com/s/files/1/0037/8008/3782/products/avengers_infinity_war_2_framed1.jpg?v=1556737096"></Image>
+    },
+    {
+        name: "Ip Man",
+        year: "2008",
+        director: " Wilson Yip",
+        picture: <Image src ="https://lh3.googleusercontent.com/proxy/jsaBv8ZMqlaMg-n1zyVY-vgS4NUiOkfASFjEWahLO9xsLenQJIyna4uTgOlB1aFshSUaSXJrzdTEmFPQDO7IYP1n4zn7FPmY9lwXK2D5ZKEWpDr35WTf"></Image>
+    },
+    {
+        name: "The Karate Kid",
+        year: "1984",
+        director: "John G. Avildsen",
+        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/511xtDXSK1L.__AC_SY300_QL70_ML2_.jpg"></Image>
+    },
+    {
+        name: "Back to the Future",
+        year: "1985",
+        director: " Robert Zemeckis",
+        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/71pwYomGC1L._AC_SY741_.jpg"></Image>
+    },
+
+]
