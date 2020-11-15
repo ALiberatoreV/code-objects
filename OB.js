1fffd51fdaee994f0b39e183feba406b6da8ea3f
ALiberatoreV <aliberatore@mail.valenciacollege.edu>
2020-11-15 18:39:40 -0500

    2

--- OB.js
+++ OB.js
@@ -3,31 +3,48 @@ let topMovies = [
         name: "The Empire Strikes Back",
         year: "1980",
         director: " Irvin Kershner",
-        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/815oLrM3hqL._AC_SY879_.jpg"></Image>
+        picture: "https://images-na.ssl-images-amazon.com/images/I/815oLrM3hqL._AC_SY879_.jpg"
     },
     {
         name: "Avengers: Infinity War",
         year: "2018",
         director: "Anthony Russo, Joe Russo",
-        picture: <Image src ="https://cdn.shopify.com/s/files/1/0037/8008/3782/products/avengers_infinity_war_2_framed1.jpg?v=1556737096"></Image>
+        picture: "https://cdn.shopify.com/s/files/1/0037/8008/3782/products/avengers_infinity_war_2_framed1.jpg?v=1556737096"
     },
     {
         name: "Ip Man",
         year: "2008",
         director: " Wilson Yip",
-        picture: <Image src ="https://lh3.googleusercontent.com/proxy/jsaBv8ZMqlaMg-n1zyVY-vgS4NUiOkfASFjEWahLO9xsLenQJIyna4uTgOlB1aFshSUaSXJrzdTEmFPQDO7IYP1n4zn7FPmY9lwXK2D5ZKEWpDr35WTf"></Image>
+        picture: "https://lh3.googleusercontent.com/proxy/jsaBv8ZMqlaMg-n1zyVY-vgS4NUiOkfASFjEWahLO9xsLenQJIyna4uTgOlB1aFshSUaSXJrzdTEmFPQDO7IYP1n4zn7FPmY9lwXK2D5ZKEWpDr35WTf"
     },
     {
         name: "The Karate Kid",
         year: "1984",
         director: "John G. Avildsen",
-        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/511xtDXSK1L.__AC_SY300_QL70_ML2_.jpg"></Image>
+        picture: "https://images-na.ssl-images-amazon.com/images/I/511xtDXSK1L.__AC_SY300_QL70_ML2_.jpg"
     },
     {
         name: "Back to the Future",
         year: "1985",
         director: " Robert Zemeckis",
-        picture: <Image src ="https://images-na.ssl-images-amazon.com/images/I/71pwYomGC1L._AC_SY741_.jpg"></Image>
+        picture: "https://images-na.ssl-images-amazon.com/images/I/71pwYomGC1L._AC_SY741_.jpg"
     },
 
 ]
+for(i = 0; i < topMovies.length; i++){
+
+}
+console.log(topMovies);
+
+let movielist = document.createElement(`li`);
+ol.textcontent = topMovies
+ol.appendchild(li)
+ol.appendchild(li)
+ol.appendchild(li)
+ol.appendchild(li)
+ol.appendchild(li)
+
+// let topMovies = topMovies.map(function (value, index, array)
+
+
+
